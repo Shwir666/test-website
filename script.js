6fdc35845b708cad8347ca03434f033430dc0dd0
Shwir666 <maximykolaichuk@gmail.com>
2024-12-22 15:19:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
+
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
